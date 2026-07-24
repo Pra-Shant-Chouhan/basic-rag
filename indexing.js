88da80cd0f43206e692b3b011dfbc3c2a3a8a69f
Prashant Chouhan <pchouhan122@gmail.com>
2026-07-24 23:01:40 +0530

    feat: INdexing of pdf with steps 1. Chunking -> 2. embedding -> 3. vector store(db)

--- indexing.js
+++ indexing.js
@@ -1,21 +1,21 @@
 import { PDFLoader } from '@langchain/community/document_loaders/fs/pdf';
 import { OpenAIEmbeddings } from '@langchain/openai';
 import { QdrantVectorStore } from '@langchain/qdrant';
-
+import 'dotenv/config';
 async function generateVectorEmbeddingsForFile(filepath) {
     const loader = new PDFLoader(filepath)
-    const document = await loader.load() //Already chunks data page by page
+    const document = await loader.load() // 1.Already chunks data page by page
 
-    // Initialize the embedding model
+    //2. Initialize the embedding model
     const embeddings = new OpenAIEmbeddings({
         model: "text-embedding-3-small",
-        apiKey: ""
+        apiKey: process.env.OPENAI_API_KEY
     });
 
-    // The vector store
+    //3. The vector store
     const vectorStore = await QdrantVectorStore.fromExistingCollection(
+        embeddings,// use this embedding model
         {
-            embeddings, // use this embedding model
             url: "http://localhost:6333",
             collectionName: "chai-code-docs"
         }
